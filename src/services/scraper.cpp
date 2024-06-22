9f43266f555670a11dcd0ad6ffbdb985882f465b
Blood-Pirouette <bloodpirouette@tutanota.com>
2024-06-21 20:10:31 -0500

    TODO: embed python

--- src/services/scraper.cpp
+++ src/services/scraper.cpp
@@ -4,11 +4,14 @@
 #include <algorithm>
 #include <iostream>
 #include <string.h>
+#include <python3.11/Python.h>
 
 using namespace std;
 
 string getTitle(const char *markup)
 {
-    //TODO: Implement cpython-vita interpreter with Beatiful Soup 4
+    // TODO: Implement cpython-vita interpreter with Beatiful Soup 4
+    //Py_Initialize();
+    //PyRun_SimpleString("print('Hello world')\n");
     return "";
 }
