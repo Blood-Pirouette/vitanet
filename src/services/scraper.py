cd0011fab487a2380b1b0281b0a1ab032a0b4d18
Blood-Pirouette <bloodpirouette@tutanota.com>
2024-06-29 00:30:19 -0500

    Early BS4 implementation

--- src/services/scraper.py
+++ src/services/scraper.py
@@ -1,1 +1,18 @@
-my_string = "Hello from Python"
+from bs4 import BeautifulSoup
+
+#Declare variable
+my_string = ''
+
+# Example HTML
+html_string = '<html><head><title>Example Page</title></head><body><h1>Hello, BeautifulSoup!</h1></body></html>'
+
+# Create a BeautifulSoup object
+soup = BeautifulSoup(html_string, 'html.parser')
+
+# Extract the title of the webpage
+title_object = soup.title
+if title_object:
+    my_string = title_object.text
+    my_string = my_string.encode('utf-8')
+else:
+    my_string = 'no title found'
