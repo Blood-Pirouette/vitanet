d052aa50f78239a09050057612a9646177680b4e
Blood-Pirouette <bloodpirouette@tutanota.com>
2024-07-15 12:21:45 -0400

    extracted_category_items implementation containing the catrgory title and url

--- src/services/scraper.py
+++ src/services/scraper.py
@@ -1,16 +1,17 @@
 from bs4 import BeautifulSoup
 
 
-
 # function to get categories
 def get_categories(markup):
     # The variable markup exists in the global namespace before execution
     soup = BeautifulSoup(markup, 'html.parser')
     categories = soup.find('ul', class_='nav nav-list')
     category_items = categories.find_all('li')
-    category_items_text = []
+    extracted_category_items = []
     for item in category_items:
         cleaned_text = ' '.join(item.text.split())
-        category_items_text.append(cleaned_text)
-    return category_items_text
+        a_tag = item.find('a')
+        url_link = a_tag.get('href')
+        extracted_category_items.append((cleaned_text,url_link))
+    return extracted_category_items
 
