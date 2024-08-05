a52f5f72e8620d1fba82aadd07b4f273fa58b480
Blood-Pirouette <bloodpirouette@tutanota.com>
2024-08-04 20:47:24 -0500

    early implementation of recieve_article_from_python

--- src/services/scraper.cpp
+++ src/services/scraper.cpp
@@ -5,7 +5,7 @@ using namespace std;
 string recieve_search_results_from_python(const char *markup, vector<Search_Result> *search_results, PyObject *p_main_module)
 {
 	// get the string variable from the global dictionary
-	PyObject *p_func = PyObject_GetAttrString(p_main_module, "get_search_results");
+	PyObject *p_func = PyObject_GetAttrString(p_main_module, "get_article");
 	PyObject *p_markup = PyString_FromString(markup);
 
 	if (p_markup == nullptr)
@@ -23,6 +23,7 @@ string recieve_search_results_from_python(const char *markup, vector<Search_Resu
 
 		// call function
 		PyObject *p_search_result_list = PyObject_CallObject(p_func, p_args);
+
 		// convert the Python list to a C++ vector of strings
 		for (int i = 0; i < PyList_Size(p_search_result_list); ++i)
 		{
@@ -57,6 +58,62 @@ string recieve_search_results_from_python(const char *markup, vector<Search_Resu
 string recieve_article_from_python(const char *markup, map<string, vector<string>> *article, PyObject *p_main_module)
 {
 
+	// get the string variable from the global dictionary
+	PyObject *p_func = PyObject_GetAttrString(p_main_module, "get_search_results");
+	PyObject *p_markup = PyString_FromString(markup);
+
+	if (p_markup == nullptr)
+	{
+		Py_Finalize();
+		return "Failed to create Python string from C++ string";
+	}
+
+	// check p_func exists and is a callable function
+	if (p_func && PyCallable_Check(p_func))
+	{
+		PyObject *p_markup = PyString_FromString(markup);
+		PyObject *p_args = PyTuple_New(1);
+		PyTuple_SetItem(p_args, 0, p_markup);
+
+		// call function
+		PyObject *p_article = PyObject_CallObject(p_func, p_args);
+
+		// PyObject dictionary variables
+		PyObject *p_key;
+		PyObject *p_value;
+		Py_ssize_t *pos;
+
+		// loop through the dictionary and extract the content
+		while (PyDict_Next(p_article, pos, &p_key, &p_value))
+		{
+			vector<string> values; // vector to store the paragraphs
+			for (int i = 0; i < PyList_Size(p_value); i++)
+			{
+				// convert paragraphs under each header
+				PyObject *c_paragraph_utf8 = PyUnicode_AsUTF8String(p_value);
+				const char *c_paragraph = PyString_AsString(c_paragraph_utf8);
+				string cpp_paragraph = c_paragraph;
+				values.push_back(c_paragraph);
+			}
+
+			// convert header of each paragraph
+			PyObject *c_key_utf8 = PyUnicode_AsUTF8String(p_key);
+			const char *c_key = PyString_AsString(c_key_utf8);
+			string cpp_key = c_key;
+
+			// store the header and the corresponding paragraphs
+			(*article)[c_key] = values;
+			values.clear();
+		}
+		// convert the Python list to a C++ vector of strings
+
+		return "Success";
+	}
+	else
+	{
+		return "Failed to get data from the html file";
+	}
+	return "";
 }
 
 string getSearchResults(vector<Search_Result> *search_results, const char *markup)
