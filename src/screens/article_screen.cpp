383822d2a4fd1d1f1e003993f1377c2c13d80f97
Blood-Pirouette <bloodpirouette@tutanota.com>
2024-08-11 14:27:36 -0500

    control map iterator

--- src/screens/article_screen.cpp
+++ src/screens/article_screen.cpp
@@ -14,17 +14,20 @@ string textWrap(string paragraph, int location, int constant)
 	else
 	{
 		paragraph.at(n) = '\n';
-		return textWrap(paragraph, location+constant, constant);
+		return textWrap(paragraph, location + constant, constant);
 	}
 }
 
 void articleScreen(map<string, vector<string>> *article)
 {
-
+	// declare local variables
+	SceCtrlData pad; // monitor trackpad presses
 	vita2d_font *text_font = vita2d_load_font_file("app0:assets/font.ttf");
+	map<string, vector<string>>::iterator it = article->begin(); // initialize an iterator
+	int p_i = 0;												 // initialize a paragraph tracker
 
 	// continuously draw choices and keep track of selection
-	while (true)
+	while (it != article->end())
 	{
 
 		// every frame draw the headers
@@ -32,26 +35,29 @@ void articleScreen(map<string, vector<string>> *article)
 		vita2d_clear_screen();
 		vita2d_draw_line(0.0, 100.0, 960.0, 100.0, TEXT_COLOR); // header split
 
-		int y_pos = 0; // initialize y_pos as 0
-		map<string, vector<string>>::iterator it = article->begin(); // create an iterator
+		// monitor vita pad
+		sceCtrlPeekBufferPositive(0, &pad, 1);
+		if (pad.buttons & SCE_CTRL_CROSS)
+		{
+			if (p_i == (it->second).size() - 1)
+			{
+				it++;	 // increment the iterator
+				p_i = 0; // reset the paragraph tracker
+			}
+			else
+			{
+				p_i++; // increment the paragraph tracker
+			}
+		}
 
 		// draw the header in the page
 		string header = it->first;
 		vita2d_font_draw_text(text_font, 30, 70, TEXT_COLOR, 40, header.c_str());
 
-		// draw the first paragraph of the header
-		string raw_paragraph = (it->second)[0];
+		// draw the paragraph
+		string raw_paragraph = (it->second)[p_i];
 		string wrapped_paragraph = textWrap(raw_paragraph, 80, 80);
 		vita2d_font_draw_text(text_font, 10, 140, TEXT_COLOR, 26, wrapped_paragraph.c_str());
-
-		/*
-		for (it; it != article->end(); it++)
-		{
-			string key = it->first;
-			vita2d_font_draw_text(text_font, 50, 130 + y_pos * 50, TEXT_COLOR, 20, key.c_str());
-			y_pos++;
-		}
-		*/
 
 		// end drawing and swap buffers
 		vita2d_end_drawing();
