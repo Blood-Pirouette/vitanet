e0008d82ec5b54f732dfece2ac308b1457875c68
Blood-Pirouette <bloodpirouette@tutanota.com>
2024-09-09 10:26:04 -0400

    1) Modified condition for incrementing paragraph. 2) Added documentation for functions.

--- src/screens/article_screen.hpp
+++ src/screens/article_screen.hpp
@@ -15,7 +15,24 @@
 
 #define TEXT_COLOR RGBA8(0x00, 0x00, 0x00, 0xFF)
 
+
+/*
+Recursive wrap text function
+
+Takes two inputs:
+1) The text to be wrapped as string
+2) The number of words per line as two parameters
+
+returns the wrapped text
+*/
 string textWrap(string, int, int);
+
+/*
+Draws the article on the screen
+
+Takes one input:
+1) A collection of header and corresponding paragraph collections in a vector<pair> format
+*/
 void articleScreen(vector<pair<string, vector<string>>> *);
 
 #endif
